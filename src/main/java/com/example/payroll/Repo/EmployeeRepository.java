952234b4edfb5dd95470329de2bba562b8efbf06
Tay Joc Cing <joccing@gmail.com>
2022-10-29 17:44:30 +0800

    Added @Component to emphasize DI by Spring Container

--- src/main/java/com/example/payroll/Repo/EmployeeRepository.java
+++ src/main/java/com/example/payroll/Repo/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.example.payroll.Repo;
 
 import com.example.payroll.Models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
 
